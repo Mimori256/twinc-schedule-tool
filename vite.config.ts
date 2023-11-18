e1a52b17a8c892859148e1132319249c5ffacf84
Mimori256 <mimorimori256@gmail.com>
2023-11-18 14:58:15 +0900

    fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "https://mimori256.github.io/twinc-schedule-tool/",
   build: {
     outDir: './dist',
   }
